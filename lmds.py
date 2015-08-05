fed0142e7c8d9611c812eea6b6344605130df2dc
clancyian <clancyian@gmail.com>
2015-08-05 12:06:51 +0100

    Initial import of App

---
+++ lmds.py
@@ -0,0 +1,56 @@
+import web
+from web import form
+import boto
+from boto.s3.key import Key
+import boto.s3.connection
+from config import *
+
+render = web.template.render('templates/')
+
+urls = (
+    '/', 'index'
+)
+
+myform = form.Form(
+    form.Textbox('image',
+        form.notnull))
+
+class index:
+    def GET(self):
+        form = myform()
+        return render.formtest(form)
+
+    def POST(self):
+        form = myform()
+        if not form.validates():
+            return render.formtest(form)
+        else:
+
+            conn = boto.s3.connect_to_region(region,
+                aws_access_key_id = access_key,
+                aws_secret_access_key = secret_key,
+                is_secure=True,               # uncomment if you are not using ssl
+                calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+            )
+
+            bucket = conn.get_bucket(bucket_name)
+
+            rs = bucket.list(prefix=form.d.image)
+
+            for key in rs:
+                myfile = bucket.get_key(key.name)
+                myfile.get_contents_to_filename(key.name)
+
+                url = conn.generate_url(
+                    60,
+                    'GET',
+                    bucket_name,
+                    key.name,
+                    )
+
+                return render.formresult(url)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
